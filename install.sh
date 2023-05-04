a572067728b05855fe5e13fbbcf838783a223223
Elana Kopelevich <elana.kopelevich@shopify.com>
2023-05-04 13:28:12 -0600

    add pretty ts error plugin for vscode

--- install.sh
+++ install.sh
@@ -21,3 +21,6 @@ ln -vsfn ~/dotfiles/.zshrc ~/.zshrc
 
 echo -e "Update path..."
 PATH=~/dotfiles/bin:$PATH
+
+echo -e "Installing VS Code plugins..."
+code --install-extension yoavbls.pretty-ts-errors
